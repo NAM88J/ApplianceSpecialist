c26f7cc26562edcbc44d91d3728d3688a3f627e1
Anatolit Novakovskiy <anatoliy.n25@gmail.com>
2023-01-05 20:12:41 +0200

    Fix tab

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).on('click', '.foryou_list.tab li', function(e){
     e.preventDefault();
     let all_tab = $('.foryou_list.tab li');
-    all_tab.removeClass('active');
-    $(this).addClass('active');
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    } else {
+        all_tab.removeClass('active');
+        $(this).addClass('active');
+    }
 });
 $(document).on('click', '.mob_button', function(e){
     e.preventDefault();
