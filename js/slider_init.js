5019c062a29596a9f346ea9cb686bb9e0593a55f
Anatolit Novakovskiy <anatoliy.n25@gmail.com>
2023-01-11 12:28:07 +0200

    Fix

--- js/slider_init.js
+++ js/slider_init.js
@@ -3,14 +3,14 @@ $('.reviews_full').slick({
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-
+    asNavFor: '.reviews_nav, reviews_content-des'
 });
 $('.reviews_content-des').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     fade: true,
-    asNavFor: '.reviews_nav',
+    asNavFor: '.reviews_nav, .reviews_full',
     prevArrow: '<div class="slider_prew slider_arrow">',
     nextArrow: '<div class="slider_next slider_arrow">',
 
